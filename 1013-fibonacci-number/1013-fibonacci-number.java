4e3a745f98abb5ed680de2b5efc9995b38af7e65
nikhil11012 <133733930+nikhil11012@users.noreply.github.com>
2025-08-08 22:52:28 +0530

    Time: 0 ms (100%), Space: 40.5 MB (48.95%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,27 +1,27 @@
-class Solution{
-    public int fib(int n){
-        if(n<=1){
-            return n;
-        }
-        return fib(n-1)+fib(n-2);
-    }
-}
-
-//done with memoization
-
-// class Solution {
-//     HashMap<Integer,Integer> hs = new HashMap<>();
-//     public int fib(int n) {
-//         if(hs.containsKey(n)){
-//             return hs.get(n);
-//         }
+// class Solution{
+//     public int fib(int n){
 //         if(n<=1){
 //             return n;
 //         }
-
-//         int result = fib(n-1) + fib(n-2);
-//         hs.put(n,result);
-
-//         return result;
+//         return fib(n-1)+fib(n-2);
 //     }
 // }
+
+//done with memoization
+
+class Solution {
+    HashMap<Integer,Integer> hs = new HashMap<>();
+    public int fib(int n) {
+        if(hs.containsKey(n)){
+            return hs.get(n);
+        }
+        if(n<=1){
+            return n;
+        }
+
+        int result = fib(n-1) + fib(n-2);
+        hs.put(n,result);
+
+        return result;
+    }
+}
