313c6cdc0fc2125be1ad2f8bdc9a2a28af985f40
nikhil11012 <133733930+nikhil11012@users.noreply.github.com>
2025-08-08 18:49:44 +0530

    Time: 0 ms (100%), Space: 40.5 MB (50.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,3 +1,12 @@
+// class solution{
+//     public int climbstairs(int n){
+//         if(n<=1){
+//             return 1;
+//         }
+//         return climbstairs(n-1) + climbstairs(n-2);
+//     }
+// } exceeds time limit so thats why we use memoization in next soln here
+
 class Solution {
     HashMap<Integer,Integer> hs = new HashMap<>();
     public int climbStairs(int n) {
